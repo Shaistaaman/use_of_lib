94dbc84dadf8836a18c2672961d07e7fc89e3b31
Shaistaaman <57103671+Shaistaaman@users.noreply.github.com>
2020-01-17 22:45:01 +0500

    Initial Code

---
+++ myweather_app/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod weather_2020
+{
+    pub mod summer
+    {
+              
+        pub fn value () 
+        {
+            println!("Summer goes upto 40C :: from sub module");
+        }
+    }
+    pub fn value_out () 
+        {
+            println!("From main module");
+        }
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use myweather_app;
+fn main()
+{
+        
+    myweather_app::weather_2020::summer::value();  
+    myweather_app::weather_2020::value_out();
+  
+}
